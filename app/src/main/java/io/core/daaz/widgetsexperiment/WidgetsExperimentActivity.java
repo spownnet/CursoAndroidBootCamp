3c6192fd254a0fe73068e0d6e1b7fbe065b331a2
Edgardo A. Palacio Joseph <spownnet@gmail.com>
2018-09-05 21:58:49 -0600

    Initial commit

---
+++ app/src/main/java/io/core/daaz/widgetsexperiment/WidgetsExperimentActivity.java
@@ -0,0 +1,13 @@
+package io.core.daaz.widgetsexperiment;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class WidgetsExperimentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_widgets_experiment);
+    }
+}
